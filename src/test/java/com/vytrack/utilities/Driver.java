c10d3f8b205abc91f952b82638ec1d0ea5dd3c6c
Yasemin0955 <ymacit0955@gmail.com>
2022-02-14 12:49:56 +0000

    updating driver class

--- src/test/java/com/vytrack/utilities/Driver.java
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -12,6 +12,8 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class Driver {
+
+    //test final branch
     private Driver() {}
 
     private static WebDriver driver;
